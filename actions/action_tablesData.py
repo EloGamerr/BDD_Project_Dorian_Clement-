9ddf51b711519ac995cfb05fb2e7fb198c5062bc
corbillc <clement.corbille@hotmail.com>
2020-11-28 15:13:03 +0100

    Merge

--- actions/action_tablesData.py
+++ actions/action_tablesData.py
@@ -45,5 +45,6 @@ class AppTablesData(QDialog):
         # TODO 1.2b : ajouter l'affichage des éléments de la vue LesSportifs après l'avoir créée
         self.refreshTable(self.ui.label_sportifs, self.ui.tableSportifs, "SELECT numSp, nomSp, prenomSp, pays, categorieSp, strftime('%d/%m/%Y',dateNaisSp), ageSp FROM LesSportifs")
         # TODO 1.3d : afficher le contenu de la table LesDisciplines et ajouter l'attribut discipline dans l'affichage de la table LesEpreuves
+        self.refreshTable(self.ui.label_disciplines, self.ui.tableDisciplines, "SELECT nomDi FROM LesDisciplines")
         # TODO 1.4b : ajouter l'affichage des éléments de la vue LesEquipes après l'avoir créée
         self.refreshTable(self.ui.label_equipes, self.ui.tableEquipes, "SELECT numEq, nbEquipiersEq FROM LesEquipes")
