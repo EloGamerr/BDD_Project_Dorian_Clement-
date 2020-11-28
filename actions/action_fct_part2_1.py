92f5c9b29877d3241773e25ce774d4fa7ad020a0
EloGamerr <elgamerr@outlook.fr>
2020-11-28 16:00:03 +0100

    TODO 2.1

---
+++ actions/action_fct_part2_1.py
@@ -0,0 +1,30 @@
+
+import sqlite3
+from utils import display
+from PyQt5.QtWidgets import QDialog
+from PyQt5.QtCore import pyqtSlot
+from PyQt5 import uic
+
+# Classe permettant d'afficher la fonction fournie 1
+class AppFctPart21(QDialog):
+
+    # Constructeur
+    def __init__(self, data:sqlite3.Connection):
+        super(QDialog, self).__init__()
+        self.ui = uic.loadUi("gui/fct_part2_1.ui", self)
+        self.data = data
+        self.refreshResult()
+
+    # Fonction de mise à jour de l'affichage
+    @pyqtSlot()
+    def refreshResult(self):
+
+        display.refreshLabel(self.ui.label_fct_part2_1, "")
+        try:
+            cursor = self.data.cursor()
+            result = cursor.execute("SELECT numEq, AVG(ageSp) FROM LesEquipiers JOIN LesSportifs USING (numSp) GROUP BY numEq HAVING numEq IN (SELECT gold FROM LesResultats)")
+        except Exception as e:
+            self.ui.table_fct_part2_1.setRowCount(0)
+            display.refreshLabel(self.ui.label_fct_part2_1, "Impossible d'afficher les résultats : " + repr(e))
+        else:
+            display.refreshGenericData(self.ui.table_fct_part2_1, result)
